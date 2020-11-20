b49dd48f93252e2bff53929dd90cf1c2c6b71d99
pedrozonta21 <pedrozonta6@gmail.com>
2020-11-19 22:13:19 -0300

    Manipulating a string and make it seems a string of type time

--- split_string.php
+++ split_string.php
@@ -1,4 +1,11 @@
 <?php
 
 $time = '082307';
-echo $time;
+
+//Make chunks of $time (chunk with size 2 characters)
+$timeSplit = str_split($time, 2);
+
+//Combine the chunks separated with : to seem a time string
+$timeSplit = implode(':', $timeSplit);
+
+echo $timeSplit;
