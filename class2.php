e8aaea733d5379c5d5b27c9bc9abf2ea84db9386
pedrozonta21 <pedrozonta6@gmail.com>
2020-12-03 21:22:55 -0300

    Calculating the difference of two dates

--- class2.php
+++ class2.php
@@ -1,6 +1,7 @@
 <?php 
 	$datetime1 = new DateTime('1981-11-03');
 	$datetime2 = new DateTime('2013-09-04');
+
 	$interval = $datetime1->diff($datetime2);
 
 	echo $interval->format('%y years, %m months, %d days');
