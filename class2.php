de27240d07f1a73c5322d7d88281de1138fee26d
Gabriel Rodrigues <gabriel.desenvolve@gmail.com>
2020-12-03 21:27:51 -0300

    Commenting on operations

--- class2.php
+++ class2.php
@@ -1,8 +1,9 @@
-<?php 
-	$datetime1 = new DateTime('1981-11-03');
-	$datetime2 = new DateTime('2013-09-04');
+<?php
+//Create var date
+$datetime1 = new DateTime('1981-11-03');
+$datetime2 = new DateTime('2013-09-04');
 
-	$interval = $datetime1->diff($datetime2);
-
-	echo $interval->format('%y years, %m months, %d days');
- ?>
+// Returns the difference between two DateTime objects represented as a DateInterval.
+$interval = $datetime1->diff($datetime2);
+// Show the intervals
+echo $interval->format('%y years, %m months, %d days');
